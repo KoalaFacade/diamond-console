5bb147ccdbd9bb9ac84c2cbab25a7f8cba75eae0
holiq <holiq.ibrahim376@gmail.com>
2023-10-12 11:14:44 +0700

    refactor(command): query builder structure directory

--- src/Commands/Domain/BuilderMakeCommand.php
+++ src/Commands/Domain/BuilderMakeCommand.php
@@ -33,8 +33,8 @@ class BuilderMakeCommand extends Command implements Console
     {
         return Source::resolveNamespace(
             data: new NamespaceData(
-                structures: Source::resolveDomainPath(),
-                domainArgument: 'Shared\\' . $this->resolveDomainArgument(),
+                domainArgument: $this->resolveDomainArgument(),
+                structures: Source::resolveDomainPath() . '\\Shared',
                 nameArgument: $this->resolveNameArgument(),
                 endsWith: 'Models\\Builders',
             )
@@ -67,8 +67,8 @@ class BuilderMakeCommand extends Command implements Console
         if ($this->resolveModelOption()) {
             $namespace = Source::resolveNamespace(
                 data: new NamespaceData(
-                    structures: Source::resolveDomainPath(),
-                    domainArgument: 'Shared\\' . $this->resolveDomainArgument(),
+                    domainArgument: $this->resolveDomainArgument(),
+                    structures: Source::resolveDomainPath() . '\\Shared',
                     nameArgument: $this->resolveModelOption(),
                     endsWith: 'Models\\' . $this->resolveModelOption(),
                 )
